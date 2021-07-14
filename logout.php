7fd1e8d5068e8390d356db665cc2beedbe7231e2
tnebes <tomislav.nebes@gmail.com>
2021-07-14 19:03:59 +0200

    Added question

--- logout.php
+++ logout.php
@@ -1,5 +1,15 @@
 <?php declare(strict_types = 1);
 
+   // why doesn't this work?
+
+   // if (isset($_SESSION['authorised']))
+   // {
+   //    session_start();
+   //    session_destroy();
+   //    unset($_SESSION);
+   //    header('location:' . $APPLICATION_PATH . 'index.php');
+   // }
+
    session_start();
    session_destroy();
    unset($_SESSION);
