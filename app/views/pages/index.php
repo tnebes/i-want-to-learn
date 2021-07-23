5c099fdd53433d0140eaeaaa36c5a97f4e95997c
tnebes <tomislav.nebes@gmail.com>
2021-07-23 15:18:07 +0200

    Added CTA

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -12,15 +12,15 @@
       <div class="wrapper-landing">
          <h1>Learn everything.</h1>
          <h2>Are you an in-depth learner?</h2>
-         <div class="landing-text">
+         <div class="landing-text" style="display: none">
             <h3>Are you looking for a specific subject?</h3>
             <h4>Are you looking for a specific course?</h4>
             <h5>Are you looking for a specific book?</h5>
             <h6>Are you looking for specific materials?</h6>
          </div>
-         <h1>With IWTL, the sky is the limit</h1>
-         <h2>IWTL is a community-driven project</h2>
+         <h3>IWTL is a community-driven project</h2>
          <h3>Read, learn and contribute.</h3>
+         <a class="large button" href="<?php echo URLROOT?>/users/register">Join now.</a>
       </div>
    </div>
    <?php require_once APPROOT . '/views/includes/footer.php'; ?>
